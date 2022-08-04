8a7c734b43d0b263444e3c7b29ac35ec8bbe3acd
Volodymyr Sennikov <vsnnkv@gmail.com>
2022-08-04 15:35:10 +0300

    Updated deposit methods, solved problems with mailSender

--- account-service/src/main/java/sen/vol/account/AccountApplication.java
+++ account-service/src/main/java/sen/vol/account/AccountApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class AccountApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(AccountApplication.class, args);
     }
 }
--- notification-service/src/main/java/sen/vol/notification/config/MailConfig.java
+++ notification-service/src/main/java/sen/vol/notification/config/MailConfig.java
@@ -11,26 +11,27 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@PropertySource({"classpath:mail-props.properties"})
+@PropertySource("classpath:mail-props.properties")
 public class MailConfig {
+
     @Autowired
     private Environment environment;
 
-    public MailConfig() {
-    }
-
     @Bean
-    public JavaMailSender javaMailSender() {
+    public JavaMailSender javaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.ukr.net");
         mailSender.setPort(465);
-        mailSender.setUsername(this.environment.getProperty("mail.username"));
-        mailSender.setPassword(this.environment.getProperty("mail.password"));
+
+        mailSender.setUsername(environment.getProperty("mail.username"));
+        mailSender.setPassword(environment.getProperty("mail.password"));
+
         Properties properties = mailSender.getJavaMailProperties();
-        properties.put("mail.transport.protocol", this.environment.getProperty("mail.transport.protocol"));
-        properties.put("mail.smtp.auth", this.environment.getProperty("mail.smtp.auth"));
-        properties.put("mail.smtp.starttls.enable", this.environment.getProperty("mail.smtp.starttls.enable"));
-        properties.put("mail.debug", this.environment.getProperty("mail.debug"));
+        properties.put("mail.transport.protocol", environment.getProperty("mail.transport.protocol"));
+        properties.put("mail.smtp.auth", environment.getProperty("mail.smtp.auth"));
+        properties.put("mail.smtp.ssl.enable", environment.getProperty("mail.smtp.ssl.enable"));
+        properties.put("mail.debug", environment.getProperty("mail.debug"));
+
         return mailSender;
     }
 }
